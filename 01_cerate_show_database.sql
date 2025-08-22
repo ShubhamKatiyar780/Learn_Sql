d1a727e80a1465008b1495f138987b05761f0e47
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-08-23 00:08:01 +0530

    create database

---
+++ 01_cerate_show_database.sql
@@ -0,0 +1,8 @@
+# correct way for create database
+CREATE DATABASE IF NOT EXISTS ecommerce;
+
+# show database
+SHOW DATABASES;
+
+# if you don't select a database, then you will have to write database name every time
+USE ecommerce;
